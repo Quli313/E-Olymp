c69d59994d0862f8aa105beba8680e97d79264f0
Ali Guliyev <107745461+Quli313@users.noreply.github.com>
2023-11-28 02:39:06 +0400

    Add files via upload

--- 500-1000/eolymp907.py
+++ 500-1000/eolymp907.py
@@ -1,15 +1,12 @@
 n = int(input())
-k = list(map(float, input().split()))
-eded=0
-t=0
-if len(k) == n:
-    for i in k:
-        t+=1
-        if 0<i<=2.5:
-            eded+=i
-            break
-    if eded>0:
-        print(f"{t} {eded:.2f}")
-    else:
-        print("Not Found")
-        ?
+setir = input()
+massiv = list(map(float, setir.split(' ')))
+
+for i in range(n):
+    deyisen = massiv[i]
+    if deyisen <= 2.5:
+        print(f"{i + 1} {deyisen:.2f}")
+        break
+
+else:
+    print("Not Found")
